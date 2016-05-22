fd1f4b4580e987e4999209e14584cb3ac786ee14
seescode <seesyong@gmail.com>
2016-05-21 20:05:42 -0400

    bomb

--- frontend/directives/tile.js
+++ frontend/directives/tile.js
@@ -10,10 +10,28 @@
             link: function (scope, element, attrs) {
                 scope.state = 'closed';   
                 
-                scope.click = function() {
+                scope.click = function() {                                       
                     scope.state = 'opened';
-                    scope.openedValue = scope.tileValue;                    
+                    scope.openedValue = scope.tileValue;
+                    
+                    if (scope.tileValue == 'bomb') {
+                        alert('bomb!');
+                    }                    
                 }           
+                
+                element.bind('contextmenu', function(event) {
+                    scope.$apply(function() {
+                        event.preventDefault();
+                        
+                        if (scope.state == 'closed') {
+                            scope.state = 'flagged';                            
+                        }
+                        else if (scope.state == 'flagged') {
+                            scope.state = 'closed';
+                        }                        
+                    });
+                });
+                        
             }
         };
     };
--- site/game.js
+++ site/game.js
@@ -47167,10 +47167,28 @@ $provide.value("$locale", {
             link: function (scope, element, attrs) {
                 scope.state = 'closed';   
                 
-                scope.click = function() {
+                scope.click = function() {                                       
                     scope.state = 'opened';
-                    scope.openedValue = scope.tileValue;                    
+                    scope.openedValue = scope.tileValue;
+                    
+                    if (scope.tileValue == 'bomb') {
+                        alert('bomb!');
+                    }                    
                 }           
+                
+                element.bind('contextmenu', function(event) {
+                    scope.$apply(function() {
+                        event.preventDefault();
+                        
+                        if (scope.state == 'closed') {
+                            scope.state = 'flagged';                            
+                        }
+                        else if (scope.state == 'flagged') {
+                            scope.state = 'closed';
+                        }                        
+                    });
+                });
+                        
             }
         };
     };
